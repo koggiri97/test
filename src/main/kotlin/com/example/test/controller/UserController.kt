99815004ee52f8a643ecc85fcf8fdb826f297266
koggiri97 <84958769+koggiri97@users.noreply.github.com>
2023-08-07 14:42:03 +0900

    feat: remove api

--- src/main/kotlin/com/example/test/controller/UserController.kt
+++ src/main/kotlin/com/example/test/controller/UserController.kt
@@ -19,10 +19,5 @@ class UserController {
     fun getUserList() : ResponseEntity<String> {
         return ResponseEntity.ok("성공")
     }
-
-    @PostMapping("/checking/list")
-    fun setUserList() : ResponseEntity<String> {
-        return ResponseEntity.ok("성공")
-    }
-
+    
 }
